3b0c64b41d3482d23ff4d8ccc5a726d2a7441b1c
wilson.otalvaro <wotalvaro@intergrupo.com>
2025-05-08 13:15:04 -0500

    Crear Pizzas

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -5,35 +5,41 @@ using ContosoPizza.Services;
 
 namespace ContosoPizza.Pages
 {
-    
-   public class PizzaListModel : PageModel
-{
-   
-    private readonly PizzaService _service;
-    public IList<Pizza> PizzaList { get;set; } = default!;
 
-    public PizzaListModel(PizzaService service)
+    public class PizzaListModel : PageModel
     {
-        _service = service;
-    }
 
-    public void OnGet()
-    {
-        PizzaList = _service.GetPizzas();
-    }
-    [BindProperty]
-public Pizza NewPizza { get; set; } = default!;
+        private readonly PizzaService _service;
+        public IList<Pizza> PizzaList { get; set; } = default!;
+
+        public PizzaListModel(PizzaService service)
+        {
+            _service = service;
+        }
+
+        public void OnGet()
+        {
+            PizzaList = _service.GetPizzas();
+        }
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
         public IActionResult OnPost()
-{
-    if (!ModelState.IsValid || NewPizza == null)
-    {
-        return Page();
-    }
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
 
-    _service.AddPizza(NewPizza);
+            _service.AddPizza(NewPizza);
+
+            return RedirectToAction("Get");
+        }
+        public IActionResult OnPostDelete(int id)
+{
+    _service.DeletePizza(id);
 
     return RedirectToAction("Get");
 }
-}
-
+    }
+    
 }
